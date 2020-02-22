6260af5d44afdd35b757ebca8bfe5a81e2abab04
Oliver Klein <me@oliverkle.in>
2020-02-22 11:12:04 -0800

    Add animations for first load

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ const IndexPage = ({
   const [projectBreakPoints, setProjectBreakPoints] = useState(null);
   const [photoCount, setPhotoCount] = useState(null);
   const [viewportWidth, setViewportWidth] = useState(null);
+  const [rendered, setRendered] = useState(false);
   const topOfProject = useRef(null);
 
   // Handle Screen Resize
@@ -26,11 +27,12 @@ const IndexPage = ({
     window.addEventListener('resize', (e) => {
       setViewportWidth(e.target.innerWidth);
     });
+    setRendered(true);
   }, [])
 
   // Prepare Images and their MetaData
   useEffect(() => {
-    const homeImages = [{ url: 'la-skate.jpg', cover: true }];
+    const homeImages = [{ url: 'la-skate.jpg', cover: true, color: '#333' }];
     const allImages = [...homeImages];
     const breakpoints = [homeImages.length];
     let numberOfPhotos = 0;
@@ -85,7 +87,7 @@ const IndexPage = ({
     <Layout>
       <SEO title="Design & Technology Studio by Oliver Thomas Klein" />
       <Grid>
-        <Navigation projectIsOpen={projectIsOpen}>
+        <Navigation rendered={rendered} projectIsOpen={projectIsOpen}>
           <NavItem>Est. 2019</NavItem>
           <NavItem>
             <Link href="mailto:oliver@otk.studio">Contact</Link>,
@@ -94,9 +96,9 @@ const IndexPage = ({
           <NavItem>Art, Design & Technology</NavItem>
         </Navigation>
 
-        <Title ref={topOfProject} onClick={() => setCurrentSlide(0)} projectIsOpen={projectIsOpen} viewBox="0 0 1418.634 201.272"><path d="M103.972,224.336c58.936,0,92.018-41.144,92.018-100.636,0-58.658-33.082-100.636-92.018-100.636-59.77,0-92.3,41.7-92.3,100.636C11.676,188.474,45.036,224.336,103.972,224.336Zm0-21.684c-21.962,0-39.2-7.784-50.04-21.684-7.506-9.73-13.9-30.3-13.9-57.268,0-25.02,5.282-45.592,14.734-58.1,8.34-10.842,25.02-20.85,50.318-20.85,21.962,0,37.53,7.506,47.538,21.962,9.73,13.9,15.012,31.692,15.012,56.99,0,25.02-5.282,43.368-15.012,57.268C142.614,195.424,127.046,202.652,103.972,202.652ZM250.2,221h25.3V48.362h64.774V26.4H185.426V48.362H250.2Zm224.9,0h31.414L424.228,107.02,504.57,26.4H470.932l-92.574,93.686V26.4h-25.3V221h25.3V153.446l27.8-28.078Zm139.278,3.336c37.53,0,78.4-12.788,78.4-60.048,0-41.422-33.082-48.372-71.724-54.488-26.41-4.17-55.878-11.12-55.878-33.082,0-22.8,23.63-32.8,42.534-32.8,31.97,0,51.43,16.68,51.986,45.87l26.132-4.726c-4.17-46.148-37.252-61.994-77.562-61.994-31.692,0-68.944,15.012-68.944,54.766,0,37.252,33.36,47.816,63.662,55.044,29.19,6.95,63.662,6.394,63.662,33.36,0,28.356-24.464,36.974-53.1,36.974-30.024,0-52.82-17.236-52.82-48.372l-26.688,2.78C534.038,202.93,571.568,224.336,614.38,224.336ZM752.546,221h25.3V48.362h64.774V26.4H687.772V48.362h64.774Zm179.588,3.336c49.484,0,76.172-23.908,76.172-67.554V26.4h-25.3V156.782c0,30.858-23.908,45.314-50.874,45.314s-50.6-14.456-50.6-45.314V26.4h-25.3V156.782C856.24,200.428,882.65,224.336,932.134,224.336ZM1028.878,221h66.164c57.824,0,92.3-36.418,92.3-97.3,0-58.38-33.36-97.3-92.3-97.3h-66.164Zm25.02-21.684V48.084h38.364c24.186,0,43.368,7.228,52.82,20.85,9.73,13.9,13.9,29.19,13.9,54.766,0,25.3-4.17,41.144-13.9,55.044-8.9,13.066-27.522,20.572-52.82,20.572ZM1204.018,221h25.3V26.4h-25.3Zm134.274,3.336c58.936,0,92.018-41.144,92.018-100.636,0-58.658-33.082-100.636-92.018-100.636-59.77,0-92.3,41.7-92.3,100.636C1246,188.474,1279.356,224.336,1338.292,224.336Zm0-21.684c-21.962,0-39.2-7.784-50.04-21.684-7.506-9.73-13.9-30.3-13.9-57.268,0-25.02,5.282-45.592,14.734-58.1,8.34-10.842,25.02-20.85,50.318-20.85,21.962,0,37.53,7.506,47.538,21.962,9.73,13.9,15.012,31.692,15.012,56.99,0,25.02-5.282,43.368-15.012,57.268C1376.934,195.424,1361.366,202.652,1338.292,202.652Z" transform="translate(-11.676 -23.064)"/></Title>
+        <Title rendered={rendered} ref={topOfProject} onClick={() => setCurrentSlide(0)} projectIsOpen={projectIsOpen} viewBox="0 0 1418.634 201.272"><path d="M103.972,224.336c58.936,0,92.018-41.144,92.018-100.636,0-58.658-33.082-100.636-92.018-100.636-59.77,0-92.3,41.7-92.3,100.636C11.676,188.474,45.036,224.336,103.972,224.336Zm0-21.684c-21.962,0-39.2-7.784-50.04-21.684-7.506-9.73-13.9-30.3-13.9-57.268,0-25.02,5.282-45.592,14.734-58.1,8.34-10.842,25.02-20.85,50.318-20.85,21.962,0,37.53,7.506,47.538,21.962,9.73,13.9,15.012,31.692,15.012,56.99,0,25.02-5.282,43.368-15.012,57.268C142.614,195.424,127.046,202.652,103.972,202.652ZM250.2,221h25.3V48.362h64.774V26.4H185.426V48.362H250.2Zm224.9,0h31.414L424.228,107.02,504.57,26.4H470.932l-92.574,93.686V26.4h-25.3V221h25.3V153.446l27.8-28.078Zm139.278,3.336c37.53,0,78.4-12.788,78.4-60.048,0-41.422-33.082-48.372-71.724-54.488-26.41-4.17-55.878-11.12-55.878-33.082,0-22.8,23.63-32.8,42.534-32.8,31.97,0,51.43,16.68,51.986,45.87l26.132-4.726c-4.17-46.148-37.252-61.994-77.562-61.994-31.692,0-68.944,15.012-68.944,54.766,0,37.252,33.36,47.816,63.662,55.044,29.19,6.95,63.662,6.394,63.662,33.36,0,28.356-24.464,36.974-53.1,36.974-30.024,0-52.82-17.236-52.82-48.372l-26.688,2.78C534.038,202.93,571.568,224.336,614.38,224.336ZM752.546,221h25.3V48.362h64.774V26.4H687.772V48.362h64.774Zm179.588,3.336c49.484,0,76.172-23.908,76.172-67.554V26.4h-25.3V156.782c0,30.858-23.908,45.314-50.874,45.314s-50.6-14.456-50.6-45.314V26.4h-25.3V156.782C856.24,200.428,882.65,224.336,932.134,224.336ZM1028.878,221h66.164c57.824,0,92.3-36.418,92.3-97.3,0-58.38-33.36-97.3-92.3-97.3h-66.164Zm25.02-21.684V48.084h38.364c24.186,0,43.368,7.228,52.82,20.85,9.73,13.9,13.9,29.19,13.9,54.766,0,25.3-4.17,41.144-13.9,55.044-8.9,13.066-27.522,20.572-52.82,20.572ZM1204.018,221h25.3V26.4h-25.3Zm134.274,3.336c58.936,0,92.018-41.144,92.018-100.636,0-58.658-33.082-100.636-92.018-100.636-59.77,0-92.3,41.7-92.3,100.636C1246,188.474,1279.356,224.336,1338.292,224.336Zm0-21.684c-21.962,0-39.2-7.784-50.04-21.684-7.506-9.73-13.9-30.3-13.9-57.268,0-25.02,5.282-45.592,14.734-58.1,8.34-10.842,25.02-20.85,50.318-20.85,21.962,0,37.53,7.506,47.538,21.962,9.73,13.9,15.012,31.692,15.012,56.99,0,25.02-5.282,43.368-15.012,57.268C1376.934,195.424,1361.366,202.652,1338.292,202.652Z" transform="translate(-11.676 -23.064)"/></Title>
 
-        <Images id="images">
+        <Images rendered={rendered} id="images">
           {images &&
             <Carousel 
               selected={projectIsOpen === null}
@@ -109,6 +111,7 @@ const IndexPage = ({
         </Images>
 
         <Content 
+          rendered={rendered} 
           id="content"
           projectIsOpen={projectIsOpen}
           currentProjectHeight={currentProjectHeight}
@@ -186,6 +189,8 @@ const Grid = styled.div`
 `;
 
 const Navigation = styled.div`
+  transition: 0.7s cubic-bezier(0.24, 1, 0.32, 1);
+
   grid-area: n;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -195,7 +200,6 @@ const Navigation = styled.div`
   line-height: 22px;
   height: 28px;
   text-align: center;
-  transition: 0.7s cubic-bezier(0.24, 1, 0.32, 1);
   overflow: hidden;
   max-height: ${props => props.projectIsOpen !== null ? 0 : 28}px;
   opacity: ${props => props.projectIsOpen !== null ? 0 : 1};
@@ -250,13 +254,15 @@ const Link = styled.a`
 `;
 
 const Title = styled.svg`
+  opacity: ${({rendered}) => rendered ? 1 : 0};
+  transform: skewY(${({rendered}) => rendered ? 0 : 0.6}deg);
   grid-area: t;
   margin: 0;
   z-index: 4;
   background: transparent;
   width: calc(${props => props.projectIsOpen !== null ? 50 : 100}vw - ${props => props.projectIsOpen !== null ? 15 : 20}px);
   padding: ${props => props.projectIsOpen !== null ? 0 : 10}px 0 10px;
-  transition: 0.7s cubic-bezier(0.24, 1, 0.32, 1);
+  transition: width 0.7s cubic-bezier(0.24, 1, 0.32, 1), padding 0.7s cubic-bezier(0.24, 1, 0.32, 1), opacity 3s cubic-bezier(0.24, 1, 0.32, 1), transform 4s cubic-bezier(0.24, 1, 0.32, 1);
   @media (max-width: 800px) {
     padding: 8px 10px 4px;
     padding-bottom: 5px;
@@ -266,6 +272,8 @@ const Title = styled.svg`
 `;
 
 const Content = styled.div`
+  transition: opacity 1.2s cubic-bezier(0.24, 1, 0.32, 1) 0.6s;
+  opacity: ${({rendered}) => rendered ? 1 : 0};
   grid-area: c;
   z-index: 4;
   position: relative;
@@ -319,6 +327,8 @@ const WorkContainer = styled.div`
 `;
 
 const Images = styled.div`
+  transition: 1.2s cubic-bezier(0.24, 1, 0.32, 1) 0.6s;
+  opacity: ${({rendered}) => rendered ? 1 : 0};
   overflow: scroll;
   grid-column-start: 3 !important;
   grid-column-end: 5;
